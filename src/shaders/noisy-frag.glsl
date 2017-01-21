d09a858cf52012ffa6710951220b40b63ef2a96a
emily-vo <emvo@seas.upenn.edu>
2017-01-21 00:16:10 -0500

    Finish basic impl

--- src/shaders/noisy-frag.glsl
+++ src/shaders/noisy-frag.glsl
@@ -1,7 +1,9 @@
 varying vec2 vUv;
 varying float noise;
 varying vec4 colorV;
+uniform sampler2D image;
 void main() {
-    vec2 uv = vec2(1,1) - vUv;
-    gl_FragColor = vec4( colorV.rgb, 1.0 );
+    vec2 uv = vec2(noise);
+    vec4 color = texture2D( image, uv );
+    gl_FragColor = vec4( color.rgb, 1.0 );
 }
--- src/shaders/noisy-vert.glsl
+++ src/shaders/noisy-vert.glsl
@@ -1,11 +1,19 @@
 varying vec2 vUv;
 varying vec4 colorV;
 uniform float time;
+uniform int octaves;
+uniform float magnitude;
+varying float noise;
+
 float random(float a, float b, float c) {
-    return sin (a + pow(b, 11.0) - c * 1511.0);
+    return fract(sin(dot(vec3(a, b, c), vec3(12.9898, 78.233, 78.233)))*43758.5453);
 }
 
 float lerp(float a, float b, float t) {
+    return a * (1.0 - t) + b * t;
+}
+
+vec4 lerp(vec4 a, vec4 b, float t) {
     return a * (1.0 - t) + b * t;
 }
 
@@ -14,16 +22,17 @@ float cerp(float a, float b, float t) {
     return lerp(a, b, cos_t);
 }
 
-float interpolateNoise(float x, float y, float z, float freq) {
+float interpolateNoise(float x, float y, float z) {
     float x0, y0, z0, x1, y1, z1;
     
     // Find the grid voxel that this point falls in
     x0 = floor(x);
-    x1 = x0 + 1.0*freq;
     y0 = floor(y);
-    y1 = y0 + 1.0*freq;
     z0 = floor(z);
-    z1 = z0 + 1.0*freq;
+    
+    x1 = x0 + 1.0;
+    y1 = y0 + 1.0;
+    z1 = z0 + 1.0;
     
     // Generate noise at each of the 8 points
     float FUL, FUR, FLL, FLR, BUL, BUR, BLL, BLR;
@@ -52,20 +61,26 @@ float interpolateNoise(float x, float y, float z, float freq) {
     // back lower right
     BLR = random(x1, y0, z0);
     
-    // Lerp in the x direction
+    // Find the interpolate t values
     float n0, n1, m0, m1, v;
-    float tx = x - x0;
-    float ty = y - y0;
-    float tz = z - z0;
+    float tx = fract(x - x0);
+    float ty = fract(y - y0);
+    float tz = fract(z - z0);
+    tx = (x - x0);
+    ty = (y - y0);
+    tz = (z - z0);
     
-    n0 = cerp(FUL, FUR, tx);
-    n1 = cerp(FLL, FLR, tx);
+    // interpolate along x and y for back
+    n0 = cerp(BLL, BLR, tx);
+    n1 = cerp(BUL, BUR, tx);
     m0 = cerp(n0, n1, ty);
     
-    n0 = cerp(BUL, BUR, tx);
-    n1 = cerp(BLL, BLR, tx);
+    // interpolate along x and y for front
+    n0 = cerp(FLL, FLR, tx);
+    n1 = cerp(FUL, FUR, tx);
     m1 = cerp(n0, n1, ty);
     
+    // interpolate along z
     v = cerp(m0, m1, tz);
     
     return v;
@@ -74,21 +89,29 @@ float interpolateNoise(float x, float y, float z, float freq) {
 float generateNoise(float x, float y, float z) {
     float total = 0.0;
     float persistence = 1.0 / 2.0;
-    for (int i = 0; i < 8; i++) {
+    int its = 0;
+    for (int i = 0; i < 32; i++) {
+        if (its >= octaves) {
+            break;
+        }
+        its++;
         float freq = pow(2.0, float(i));
         float ampl = pow(persistence, float(i));
-        total += interpolateNoise(x, y, z, freq)*ampl;
+        total += interpolateNoise(freq*x, freq*y, freq*z)*ampl;
     }
     return total;
 }
 
 void main() {
     vUv = uv;
-    float s = sin(time)*generateNoise(position.x, position.y, position.z);
+    float n = generateNoise(position.x, position.z, time);
+    float s = magnitude*n;
+
+    // use noise value to scale normal displacement
     vec4 offset = vec4(s*normal.x, s*normal.y, s*normal.z, 1);
     vec4 pos = vec4(position, 1) + offset;
     gl_Position = projectionMatrix * modelViewMatrix * pos;
-    colorV = vec4(normal, 1);
+    noise = n;
 }
 
 
