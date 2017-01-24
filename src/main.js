8937b8bce3f15f132e5507d257468b240b2ab5e8
Rudraksha20 <rdimple@seas.upenn.edu>
2017-01-24 16:41:27 -0500

    still not final

--- src/main.js
+++ src/main.js
@@ -3,6 +3,43 @@ const THREE = require('three'); // older modules are imported like this. You sho
 import Framework from './framework'
 import Noise from './noise'
 import {other} from './noise'
+import DAT from 'dat-gui'
+
+var adamMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      image: { // Check the Three.JS documentation for the different allowed types and values
+        type: "t", 
+        value: THREE.ImageUtils.loadTexture('./explosion.png')
+      },
+        time: {
+        type: "f",
+        value: 1.0
+    },
+        persistance_p: {
+            type: "f",
+            value: 0.5
+        },
+        audData: {
+            type: "iv1",
+            value: new Array
+        }
+        
+    },
+    vertexShader: require('./shaders/adam-vert.glsl'),
+    fragmentShader: require('./shaders/adam-frag.glsl')
+  });
+
+var timer ={ 
+    speed: 0.03
+    }
+
+var persist = {
+    persistance: 1.13
+}
+
+var audToggle = {
+    AudioToggle: false
+}
 
 // called after the scene loads
 function onLoad(framework) {
@@ -11,23 +48,16 @@ function onLoad(framework) {
   var renderer = framework.renderer;
   var gui = framework.gui;
   var stats = framework.stats;
-
+    var audio = framework.audio;
+    //var data = framework.frequencyData;
+    
   // LOOK: the line below is synyatic sugar for the code above. Optional, but I sort of recommend it.
-  // var {scene, camera, renderer, gui, stats} = framework; 
+  // var {scene, camera, renderer, gui, stat} = framework; 
 
   // initialize a simple box and material
-  var box = new THREE.BoxGeometry(1, 1, 1);
-
-  var adamMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-      image: { // Check the Three.JS documentation for the different allowed types and values
-        type: "t", 
-        value: THREE.ImageUtils.loadTexture('./adam.jpg')
-      }
-    },
-    vertexShader: require('./shaders/adam-vert.glsl'),
-    fragmentShader: require('./shaders/adam-frag.glsl')
-  });
+  //var box = new THREE.BoxGeometry(1, 1, 1);
+  var box = new THREE.IcosahedronGeometry(1,5);
+    
   var adamCube = new THREE.Mesh(box, adamMaterial);
 
   // set camera position
@@ -41,11 +71,37 @@ function onLoad(framework) {
   gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
     camera.updateProjectionMatrix();
   });
+   
+  gui.add(timer, 'speed', 0,0.05, 0.001).onChange(function(newVal1){
+     timer.speed = newVal1; 
+  });
+    
+  gui.add(persist, 'persistance', 0,2).onChange(function(newVal2){
+     persist.persistance = newVal2; 
+  }); 
+
+    gui.add(audToggle, 'AudioToggle').onChange(function(newVal3){
+     //audToggle.AudioToggle = newVal3; 
+        if(newVal3 === true) audio.play();
+        else audio.pause();
+  }); 
+    
 }
+
+  
+
+  //var gui1 = new DAT.GUI();
+  
 
 // called on frame updates
 function onUpdate(framework) {
   // console.log(`the time is ${new Date()}`);
+    
+    adamMaterial.uniforms.time.value += timer.speed;
+    adamMaterial.uniforms.persistance_p.value = persist.persistance;  
+    adamMaterial.uniforms.audData.value = Int32Array.from(framework.frequencyData);
+    //console.log(framework.frequencyData);
+    
 }
 
 // when the scene is done initializing, it will call onLoad, then on frame updates, call onUpdate
--- src/shaders/adam-frag.glsl
+++ src/shaders/adam-frag.glsl
@@ -1,13 +1,17 @@
 varying vec2 vUv;
-varying float noise;
+varying float n;
 uniform sampler2D image;
-
+//varying vec3 col;
+varying vec3 nor;
 
 void main() {
 
-  vec2 uv = vec2(1,1) - vUv;
-  vec4 color = texture2D( image, uv );
+  vec2 uv = vec2(1,1) - vUv * cos(n);
+  vec4 color = texture2D( image, uv * sin(n) );
 
   gl_FragColor = vec4( color.rgb, 1.0 );
+  
+  //gl_FragColor = vec4(abs(nor.rgb), 1.0);
 
+  //gl_FragColor = vec4(col.rgb, 1.0);
 }
