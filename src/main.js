2993d47706600212d7a0d2d14b87aa39400614ff
gracexu94 <serihppas@gmail.com>
2017-01-24 11:27:06 -0500

    transferred noise to shaders

--- src/main.js
+++ src/main.js
@@ -15,70 +15,22 @@ function onLoad(framework) {
   // LOOK: the line below is synyatic sugar for the code above. Optional, but I sort of recommend it.
   // var {scene, camera, renderer, gui, stats} = framework; 
 
-  // initialize a simple box and material
+  // initialize icosahedron 
   var icosahedron = new THREE.IcosahedronGeometry(1, 0);
-  icosahedron.computeFaceNormals();
-  icosahedron.computeVertexNormals();
-
-  var vertexOffsets = {};
-  var vertexNormals = {};
-  for (var i = 0; i < icosahedron.faces.length; i++) {
-    var vertices = icosahedron.vertices;
-    var face = icosahedron.faces[i];
-    console.log(face);
-    var v1 = vertices[face.a];
-    var v2 = vertices[face.b];
-    var v3 = vertices[face.c];
-
-    var vertexNormal1 = face.vertexNormals[0];
-    var vertexNormal2 = face.vertexNormals[1];
-    var vertexNormal3 = face.vertexNormals[2];
-
-    vertexNormals[face.a] = vertexNormal1;
-    vertexNormals[face.b] = vertexNormal2;
-    vertexNormals[face.c] = vertexNormal3;
-
-    var offset1 = Noise.generateMultiOctaveNoise(v1.x, v1.y, v1.z, 1); 
-    var offset2 = Noise.generateMultiOctaveNoise(v2.x, v2.y, v2.z, 1); 
-    var offset3 = Noise.generateMultiOctaveNoise(v3.x, v3.y, v3.z, 1); 
-
-    vertexOffsets[face.a] = offset1;
-    vertexOffsets[face.b] = offset2;
-    vertexOffsets[face.c] = offset3;
-  }
-
-  console.log(vertexOffsets);
-  console.log(vertexNormals);
-  for (var j = 0; j < icosahedron.vertices.length; j++) {
-    console.log(vertexOffsets[j]);
-    var offset = vertexOffsets[j];
-    var normal = vertexNormals[j];
-    //normal = normal.multiplyScalar(-1.0);
-
-    var offsetAlongNormal = normal.multiplyScalar(offset);
-
-    icosahedron.vertices[j] = icosahedron.vertices[j].add(offsetAlongNormal);
-  }
-  icosahedron.verticesNeedUpdate = true;
-
 
   var myMaterial = new THREE.ShaderMaterial({
     vertexShader: require('./shaders/my-vert.glsl'),
     fragmentShader: require('./shaders/my-frag.glsl')
   });
 
-  var meshMaterial = new THREE.MeshNormalMaterial();
-
-  var texturedIcosahedron = new THREE.Mesh(icosahedron, meshMaterial);
-  var helper = new THREE.VertexNormalsHelper(texturedIcosahedron);
-
+  var texturedIcosahedron = new THREE.Mesh(icosahedron, myMaterial);
+  
   // set camera position
   camera.position.set(1, 1, 2);
   camera.lookAt(new THREE.Vector3(0,0,0));
 
   scene.add(texturedIcosahedron);
-  scene.add(helper);
-
+  
   // edit params and listen to changes like this
   // more information here: https://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
   gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
--- src/shaders/my-vert.glsl
+++ src/shaders/my-vert.glsl
@@ -1,9 +1,76 @@
 
 varying vec2 vUv;
 varying vec3 vNormal;
+const int numOctaves = 1;
+
+float generateNoise1(int x, int y, int z) {
+	return fract(sin(dot(vec3(x,y,z), vec3(12.9898, 78.23, 107.0))) * 43758.5453);
+} 
+
+float linearInterpolate(float a, float b, float t) {
+	return a * (1.0 - t) + b * t;
+}
+
+float cosineInterpolate(float a, float b, float t) {
+	float cos_t = (1.0 - cos(t * 3.14159265359879323846264)) * 0.5;
+	return linearInterpolate(a, b, cos_t);
+}
+
+// given a point in 3d space, produces a noise value by interpolating surrounding points
+float interpolateNoise(float x, float y, float z) {
+	int integerX = int(floor(x));
+    float weightX = x - float(integerX);
+
+    int integerY = int(floor(y));
+    float weightY = y - float(integerY);
+
+    int integerZ = int(floor(z));
+    float weightZ = z - float(integerZ);
+
+    float v1 = generateNoise1(integerX, integerY, integerZ);
+    float v2 = generateNoise1(integerX, integerY, integerZ + 1);
+    float v3 = generateNoise1(integerX, integerY + 1, integerZ + 1);
+    float v4 = generateNoise1(integerX, integerY + 1, integerZ);
+
+    float v5 = generateNoise1(integerX + 1, integerY, integerZ);
+    float v6 = generateNoise1(integerX + 1, integerY, integerZ + 1);
+    float v7 = generateNoise1(integerX + 1, integerY + 1, integerZ + 1);
+    float v8 = generateNoise1(integerX + 1, integerY + 1, integerZ);
+
+    float i1 = cosineInterpolate(v1, v5, weightX);
+    float i2 = cosineInterpolate(v2, v6, weightX);
+    float i3 = cosineInterpolate(v3, v7, weightX);
+    float i4 = cosineInterpolate(v4, v8, weightX);
+
+    float ii1 = cosineInterpolate(i1, i4, weightY);
+    float ii2 = cosineInterpolate(i2, i3, weightY);
+
+    return cosineInterpolate(ii1, ii2 , weightZ);
+}
+
+// a multi-octave noise generation function that sums multiple noise functions together
+// with each subsequent noise function increasing in frequency and decreasing in amplitude
+float generateMultiOctaveNoise(float x, float y, float z) {
+    float total = 0.0;
+    float persistence = 1.0/2.0;
+
+    //loop for some number of octaves
+    for (int i = 0; i < numOctaves; i++) {
+        float frequency = pow(2.0, float(i));
+        float amplitude = pow(persistence, float(i));
+
+        total += interpolateNoise(x * frequency, y * frequency, z * frequency) * amplitude;
+    }
+
+    return total;
+}
 
 void main() {
     vUv = uv;
     vNormal = normal;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+    float offset = generateMultiOctaveNoise(position[0], position[1], position[2]);
+    vec3 newPosition = position + offset * normal;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
 }
